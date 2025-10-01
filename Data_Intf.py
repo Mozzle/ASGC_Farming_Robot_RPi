0cf9400d25a88f270f84a50b2f33e79367182e6e
Dylan <dycollier0@gmail.com>
2025-10-01 15:34:07 -0500

    Implement RPi-side packet definitions and packet decoding
    NOT YET TESTED
    Implemented Unix Time response (Issue #25)

--- Data_Intf.py
+++ Data_Intf.py
@@ -1,4 +1,5 @@
 import time
+from datetime import datetime
 import pigpio
 import I2C_Packets
 from subprocess import call
@@ -45,7 +46,7 @@ def i2c_loop(id, tick):
 
 
       # If the received data length does not match the expected packet size
-      if bytes_rec is not I2C_Packets.RPI_PACKET_MAX_LENGTHS[data[I2C_Packets.PACKET_ID]]:
+      if bytes_rec is not I2C_Packets.RPI_PACKET_LENGTHS[data[I2C_Packets.PACKET_ID]]:
          # Error of some kind
          print("ERROR: Packet length mismatch! Len:" + str(bytes_rec))
          ack_pkt = I2C_Packets.RPI_I2C_Packet_ACK(C_FALSE)
@@ -60,7 +61,7 @@ def i2c_loop(id, tick):
          print("Yeah!")
 
       # -------------------------- GCODE 0 PKT ID ----------------------------
-      elif data[I2C_Packets.PACKET_ID] == I2C_Packets.RPI_GCODE_0_PKT_ID and bytes_rec == I2C_Packets.RPI_PACKET_MAX_LENGTHS[I2C_Packets.RPI_GCODE_0_PKT_ID]:
+      elif data[I2C_Packets.PACKET_ID] == I2C_Packets.RPI_GCODE_0_PKT_ID and bytes_rec == I2C_Packets.RPI_PACKET_LENGTHS[I2C_Packets.RPI_GCODE_0_PKT_ID]:
          # Parse the data into the packet struct
          pkt = I2C_Packets.RPI_I2C_Packet_GCode_0(data)
 
@@ -76,7 +77,7 @@ def i2c_loop(id, tick):
             last_rec_pkt_id = I2C_Packets.RPI_GCODE_0_PKT_ID
 
       # -------------------------- GCODE 1 PKT ID ----------------------------
-      elif data[I2C_Packets.PACKET_ID] == I2C_Packets.RPI_GCODE_1_PKT_ID and bytes_rec == I2C_Packets.RPI_PACKET_MAX_LENGTHS[I2C_Packets.RPI_GCODE_1_PKT_ID]:
+      elif data[I2C_Packets.PACKET_ID] == I2C_Packets.RPI_GCODE_1_PKT_ID and bytes_rec == I2C_Packets.RPI_PACKET_LENGTHS[I2C_Packets.RPI_GCODE_1_PKT_ID]:
          # Parse the data into the packet struct
 
          if last_rec_pkt_id == I2C_Packets.RPI_GCODE_0_PKT_ID:
@@ -90,7 +91,7 @@ def i2c_loop(id, tick):
             last_rec_pkt_id = I2C_Packets.RPI_GCODE_1_PKT_ID
 
       # -------------------------- GCODE 2 PKT ID ----------------------------
-      elif data[I2C_Packets.PACKET_ID] == I2C_Packets.RPI_GCODE_2_PKT_ID and bytes_rec == I2C_Packets.RPI_PACKET_MAX_LENGTHS[I2C_Packets.RPI_GCODE_2_PKT_ID]:
+      elif data[I2C_Packets.PACKET_ID] == I2C_Packets.RPI_GCODE_2_PKT_ID and bytes_rec == I2C_Packets.RPI_PACKET_LENGTHS[I2C_Packets.RPI_GCODE_2_PKT_ID]:
          # Parse the data into the packet struct
 
          if last_rec_pkt_id == I2C_Packets.RPI_GCODE_1_PKT_ID:
@@ -104,7 +105,7 @@ def i2c_loop(id, tick):
             last_rec_pkt_id = I2C_Packets.RPI_GCODE_2_PKT_ID
 
       # -------------------------- GCODE 3 PKT ID ----------------------------
-      elif data[I2C_Packets.PACKET_ID] == I2C_Packets.RPI_GCODE_3_PKT_ID and bytes_rec == I2C_Packets.RPI_PACKET_MAX_LENGTHS[I2C_Packets.RPI_GCODE_3_PKT_ID]:
+      elif data[I2C_Packets.PACKET_ID] == I2C_Packets.RPI_GCODE_3_PKT_ID and bytes_rec == I2C_Packets.RPI_PACKET_LENGTHS[I2C_Packets.RPI_GCODE_3_PKT_ID]:
          # Parse the data into the packet struct
 
          if last_rec_pkt_id == I2C_Packets.RPI_GCODE_2_PKT_ID:
@@ -118,7 +119,7 @@ def i2c_loop(id, tick):
             last_rec_pkt_id = I2C_Packets.RPI_GCODE_3_PKT_ID
 
       # -------------------------- GCODE 4 PKT ID ----------------------------
-      elif data[I2C_Packets.PACKET_ID] == I2C_Packets.RPI_GCODE_4_PKT_ID and bytes_rec == I2C_Packets.RPI_PACKET_MAX_LENGTHS[I2C_Packets.RPI_GCODE_4_PKT_ID]:
+      elif data[I2C_Packets.PACKET_ID] == I2C_Packets.RPI_GCODE_4_PKT_ID and bytes_rec == I2C_Packets.RPI_PACKET_LENGTHS[I2C_Packets.RPI_GCODE_4_PKT_ID]:
          # Parse the data into the packet struct
 
          if last_rec_pkt_id == I2C_Packets.RPI_GCODE_3_PKT_ID:
@@ -138,24 +139,102 @@ def i2c_loop(id, tick):
 
 
       # ------------------------ AHT20 DATA PKT ID -------------------------
-      elif data[I2C_Packets.PACKET_ID] == I2C_Packets.RPI_AHT20_PKT_ID:
-         print("Else!")
+      elif data[I2C_Packets.PACKET_ID] == I2C_Packets.RPI_AHT20_PKT_ID and bytes_rec == I2C_Packets.RPI_PACKET_LENGTHS[I2C_Packets.RPI_AHT20_PKT_ID]:
+         pkt = I2C_Packets.RPI_I2C_Packet_AHT20(data)
 
-      # ------------------------ WATER DATA PKT ID -------------------------
-      elif data[I2C_Packets.PACKET_ID] == I2C_Packets.RPI_WATER_DATA_PKT_ID:
-         pass
+         if pkt.valid == C_TRUE:
+            # Make and send the ACK packet
+            ack_pkt = I2C_Packets.RPI_I2C_Packet_ACK(C_TRUE)
+            s, b, d = pi.bsc_i2c(I2C_ADDR, ack_pkt.raw)
+            last_rec_pkt_id = I2C_Packets.RPI_AHT20_PKT_ID
+
+            print("Temp: " + str(pkt.temperature) + "C, Humidity: " + str(pkt.humidity) + "%")
+
+            # FORWARD THE pkt.temperature and pkt.humidity VALUES TO WEB SERVER HERE
+
+      # -------------------------- SEN0169 PKT ID --------------------------
+      elif data[I2C_Packets.PACKET_ID] == I2C_Packets.RPI_SEN0169_PKT_ID and bytes_rec == I2C_Packets.RPI_PACKET_LENGTHS[I2C_Packets.RPI_SEN0169_PKT_ID]:
+         pkt = I2C_Packets.RPI_I2C_Packet_SEN0169(data)
+
+         if pkt.valid == C_TRUE:
+            # Make and send the ACK packet
+            ack_pkt = I2C_Packets.RPI_I2C_Packet_ACK(C_TRUE)
+            s, b, d = pi.bsc_i2c(I2C_ADDR, ack_pkt.raw)
+            last_rec_pkt_id = I2C_Packets.RPI_SEN0169_PKT_ID
+
+            print("pH: " + str(pkt.pH))
+
+            # FORWARD THE pkt.pH VALUE TO WEB SERVER HERE
+
+      # -------------------------- SEN0244 PKT ID --------------------------
+      elif data[I2C_Packets.PACKET_ID] == I2C_Packets.RPI_SEN0244_PKT_ID and bytes_rec == I2C_Packets.RPI_PACKET_LENGTHS[I2C_Packets.RPI_SEN0244_PKT_ID]:
+         pkt = I2C_Packets.RPI_I2C_Packet_SEN0244(data)
+
+         if pkt.valid == C_TRUE:
+            # Make and send the ACK packet
+            ack_pkt = I2C_Packets.RPI_I2C_Packet_ACK(C_TRUE)
+            s, b, d = pi.bsc_i2c(I2C_ADDR, ack_pkt.raw)
+            last_rec_pkt_id = I2C_Packets.RPI_SEN0244_PKT_ID
+
+            print("TDS: " + str(pkt.tds) + " ppm")
+
+            # FORWARD THE pkt.tds VALUE TO WEB SERVER HERE
+
+      # -------------------------- AS7341 PKT 0 ID --------------------------
+      elif data[I2C_Packets.PACKET_ID] == I2C_Packets.RPI_AS7341_0_PKT_ID and bytes_rec == I2C_Packets.RPI_PACKET_LENGTHS[I2C_Packets.RPI_AS7341_0_PKT_ID]:
+         pkt = I2C_Packets.RPI_I2C_Packet_AS7341_0(data)
+
+         if pkt.valid == C_TRUE:
+            # Make and send the ACK packet
+            ack_pkt = I2C_Packets.RPI_I2C_Packet_ACK(C_TRUE)
+            s, b, d = pi.bsc_i2c(I2C_ADDR, ack_pkt.raw)
+            last_rec_pkt_id = I2C_Packets.RPI_AS7341_0_PKT_ID
+
+            print("AS7341 CH0: " + str(pkt.channel_0) + ", CH1: " + str(pkt.channel_1) + ", CH2: " + str(pkt.channel_2) + ", CH3: " + str(pkt.channel_3) + ", CH4: " + str(pkt.channel_4) + ", CH5: " + str(pkt.channel_5) + ", CH6: " + str(pkt.channel_6))
+
+            # FORWARD THE pkt.channel_0 - pkt.channel_6 VALUES TO WEB SERVER HERE
+            # OR STORE THEM IN GLOBAL VARIABLES TO BE SENT AFTER AS7341_1_PKT IS RECEIVED
+
+      # -------------------------- AS7341 PKT 1 ID --------------------------
+      elif data[I2C_Packets.PACKET_ID] == I2C_Packets.RPI_AS7341_1_PKT_ID and bytes_rec == I2C_Packets.RPI_PACKET_LENGTHS[I2C_Packets.RPI_AS7341_1_PKT_ID]:
+         pkt = I2C_Packets.RPI_I2C_Packet_AS7341_1(data)
+
+         if pkt.valid == C_TRUE and last_rec_pkt_id == I2C_Packets.RPI_AS7341_0_PKT_ID:
+            # Make and send the ACK packet
+            ack_pkt = I2C_Packets.RPI_I2C_Packet_ACK(C_TRUE)
+            s, b, d = pi.bsc_i2c(I2C_ADDR, ack_pkt.raw)
+            last_rec_pkt_id = I2C_Packets.RPI_AS7341_1_PKT_ID
+
+            print("AS7341 CH7: " + str(pkt.channel_7) + ", CH8: " + str(pkt.channel_8) + ", CH9: " + str(pkt.channel_9) + ", CH10: " + str(pkt.channel_10))
+
+            # FORWARD THE pkt.channel_7 - pkt.channel_10 VALUES TO WEB SERVER HERE
+            # OR STORE THEM IN GLOBAL VARIABLES TO BE SENT AFTER AS7341_0_PKT IS RECEIVED
 
       # ----------------------- BUTTONS DATA PKT ID ------------------------
-      elif data[I2C_Packets.PACKET_ID] == I2C_Packets.RPI_BUTTONS_PKT_ID:
+      elif data[I2C_Packets.PACKET_ID] == I2C_Packets.RPI_BUTTONS_PKT_ID and bytes_rec == I2C_Packets.RPI_PACKET_LENGTHS[I2C_Packets.RPI_BUTTONS_PKT_ID]:
          pass
 
       # ---------------------- NET POT STATUS PKT ID -----------------------
-      elif data[I2C_Packets.PACKET_ID] == I2C_Packets.RPI_NET_POT_STATUS_PKT_ID:
+      elif data[I2C_Packets.PACKET_ID] == I2C_Packets.RPI_NET_POT_STATUS_PKT_ID and bytes_rec == I2C_Packets.RPI_PACKET_LENGTHS[I2C_Packets.RPI_NET_POT_STATUS_PKT_ID]:
          pass
 
       # ------------------ GET AXES DATA REQUEST PKT ID --------------------
-      elif data[I2C_Packets.PACKET_ID] == I2C_Packets.RPI_GET_AXES_POS_PKT_ID:
+      elif data[I2C_Packets.PACKET_ID] == I2C_Packets.RPI_GET_AXES_POS_PKT_ID and bytes_rec == I2C_Packets.RPI_PACKET_LENGTHS[I2C_Packets.RPI_GET_AXES_POS_PKT_ID]:
          pass
+
+      # --------------------- UNIX TIME REQUEST PKT ID ---------------------
+      elif data[I2C_Packets.PACKET_ID] == I2C_Packets.RPI_UNIX_TIME_REQUEST_PKT_ID and bytes_rec == I2C_Packets.RPI_PACKET_LENGTHS[I2C_Packets.RPI_UNIX_TIME_REQUEST_PKT_ID]:
+         # Make the unix time packet
+         current_unix_time = int(time.time())
+         current_timezone = int(datetime.now().astimezone().strftime("%z") / 100)  # in hours
+
+         print("Sending Unix Time: " + str(current_unix_time) + ", TZ: " + str(current_timezone))
+         pkt = I2C_Packets.RPI_I2C_Packet_Unix_Time(current_unix_time, current_timezone)
+
+         # Send the unix time packet
+         s, b, d = pi.bsc_i2c(I2C_ADDR, pkt.raw)
+
+         last_rec_pkt_id = I2C_Packets.RPI_UNIX_TIME_REQUEST_PKT_ID
 
       # -------------------------- DEFAULT CASE ----------------------------
       else:
